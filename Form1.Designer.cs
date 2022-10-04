e898a62f972c1b505e8779d14d4cbfce8d99aa73
Gregory Bell <gregory.bell3@snhu.edu>
2022-10-04 14:12:13 -0400

    Coloring Book Shapes added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,14 @@
             this.components = new System.ComponentModel.Container();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.brushSizeBox = new System.Windows.Forms.ComboBox();
+            this.btnCat = new System.Windows.Forms.Button();
+            this.btnDog = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnActuallyDog = new System.Windows.Forms.Button();
+            this.btnTurtle = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,7 +49,10 @@
             "Blue",
             "Green",
             "Red",
-            "Yellow"});
+            "Yellow",
+            "Purple",
+            "Orange",
+            "Pink"});
             this.comboBox1.Location = new System.Drawing.Point(12, 73);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
@@ -54,16 +64,96 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.btnTurtle);
+            this.panel1.Controls.Add(this.btnActuallyDog);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.brushSizeBox);
+            this.panel1.Controls.Add(this.btnCat);
+            this.panel1.Controls.Add(this.btnDog);
             this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(145, 450);
+            this.panel1.Size = new System.Drawing.Size(153, 450);
             this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "What would you like to Draw?";
+            // 
+            // brushSizeBox
+            // 
+            this.brushSizeBox.FormattingEnabled = true;
+            this.brushSizeBox.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large"});
+            this.brushSizeBox.Location = new System.Drawing.Point(12, 46);
+            this.brushSizeBox.Name = "brushSizeBox";
+            this.brushSizeBox.Size = new System.Drawing.Size(121, 21);
+            this.brushSizeBox.TabIndex = 3;
+            this.brushSizeBox.Text = "Brush Size";
+            this.brushSizeBox.SelectedIndexChanged += new System.EventHandler(this.brushSizeBox_SelectedIndexChanged);
+            // 
+            // btnCat
+            // 
+            this.btnCat.Location = new System.Drawing.Point(12, 197);
+            this.btnCat.Name = "btnCat";
+            this.btnCat.Size = new System.Drawing.Size(121, 25);
+            this.btnCat.TabIndex = 2;
+            this.btnCat.Text = "Cat";
+            this.btnCat.UseVisualStyleBackColor = true;
+            this.btnCat.Click += new System.EventHandler(this.btnCat_Click);
+            // 
+            // btnDog
+            // 
+            this.btnDog.Location = new System.Drawing.Point(12, 167);
+            this.btnDog.Name = "btnDog";
+            this.btnDog.Size = new System.Drawing.Size(121, 24);
+            this.btnDog.TabIndex = 1;
+            this.btnDog.Text = "Penguin";
+            this.btnDog.UseVisualStyleBackColor = true;
+            this.btnDog.Click += new System.EventHandler(this.btnDog_Click);
             // 
             // toolTip1
             // 
             this.toolTip1.Tag = "";
             this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 100);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(121, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnActuallyDog
+            // 
+            this.btnActuallyDog.Location = new System.Drawing.Point(12, 228);
+            this.btnActuallyDog.Name = "btnActuallyDog";
+            this.btnActuallyDog.Size = new System.Drawing.Size(121, 23);
+            this.btnActuallyDog.TabIndex = 6;
+            this.btnActuallyDog.Text = "Dog";
+            this.btnActuallyDog.UseVisualStyleBackColor = true;
+            this.btnActuallyDog.Click += new System.EventHandler(this.btnActuallyDog_Click);
+            // 
+            // btnTurtle
+            // 
+            this.btnTurtle.Location = new System.Drawing.Point(12, 257);
+            this.btnTurtle.Name = "btnTurtle";
+            this.btnTurtle.Size = new System.Drawing.Size(121, 23);
+            this.btnTurtle.TabIndex = 7;
+            this.btnTurtle.Text = "Wizard Turtle";
+            this.btnTurtle.UseVisualStyleBackColor = true;
+            this.btnTurtle.Click += new System.EventHandler(this.btnTurtle_Click);
             // 
             // Form1
             // 
@@ -73,11 +163,12 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Paint App";
+            this.Text = "Coloring Book App";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -87,6 +178,13 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnDog;
+        private System.Windows.Forms.Button btnCat;
+        private System.Windows.Forms.ComboBox brushSizeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnActuallyDog;
+        private System.Windows.Forms.Button btnTurtle;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -49,11 +50,35 @@ namespace Paintapp
                         case "Green":
                             myBrush.Color = Color.Green;
                             break;
+                        case "Purple":
+                            myBrush.Color = Color.Purple;
+                            break;
+                        case "Pink":
+                            myBrush.Color = Color.Pink;
+                            break;
+                        case "Orange":
+                            myBrush.Color = Color.Orange;
+                            break;
                         default:
                             myBrush.Color = Color.Blue;
                             break;
                     }
-                    g.FillEllipse(myBrush, e.X, e.Y, 15, 15);
+                    switch (brushSizeBox.Text)
+                    {
+                        case "Small":
+                            g.FillEllipse(myBrush, e.X, e.Y, 4, 4);
+                            break;
+                        case "Medium":
+                            g.FillEllipse(myBrush, e.X, e.Y, 7, 7);
+                            break;
+                        case "Large":
+                            g.FillEllipse(myBrush, e.X, e.Y, 12, 12);
+                            break;
+                        default:
+                            g.FillEllipse(myBrush, e.X, e.Y, 7, 7);
+                            break;
+                    }
+                    
                 }
             }
         }
@@ -66,6 +91,154 @@ namespace Paintapp
         private void toolTip1_Popup(object sender, PopupEventArgs e)
         {
   
+        }
+
+        private void btnDog_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = base.CreateGraphics())
+            using (Pen pen = new Pen(Color.Black))
+            {
+                g.Clear(Color.White);
+                //Head
+                g.DrawEllipse(pen, 400, 120, 80, 80);
+                //Body
+                g.DrawEllipse(pen, 405, 200, 70, 150);
+                //Left Wing
+                g.DrawEllipse(pen, 400, 220, 35, 100);
+                //Right wing
+                g.DrawEllipse(pen, 445, 220, 35, 100);
+                //left foot
+                g.DrawEllipse(pen, 390, 340, 40, 20);
+                //Right foot
+                g.DrawEllipse(pen, 450, 340, 40, 20);
+                //Beak
+                g.DrawLine(pen, 430, 160, 440, 180);
+                g.DrawLine(pen, 450, 160, 440, 180);
+                //left Eye
+                g.DrawRectangle(pen, 415, 140 , 15, 6);
+                //right Eye
+                g.DrawRectangle(pen, 450, 140, 15, 6);
+            }
+        }
+
+
+        private void brushSizeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            brushSizeBox.Text = brushSizeBox.SelectedIndex.ToString();
+
+        }
+
+        private void btnCat_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = base.CreateGraphics())
+            using (Pen pen = new Pen(Color.Black))
+            {
+                g.Clear(Color.White);
+                //head
+                g.DrawEllipse(pen, 400, 120, 80, 80);
+                //body
+                g.DrawEllipse(pen, 390, 200, 100, 120);
+                //left foot
+                g.DrawEllipse(pen, 390, 310, 30, 20);
+                //right foot
+                g.DrawEllipse(pen, 460, 310, 30, 20);
+                //left hand
+                g.DrawEllipse(pen, 390, 230, 30, 20);
+                //right hand
+                g.DrawEllipse(pen, 460, 230, 30, 20);
+                //left ear
+                g.DrawLine(pen, 410, 130, 420, 100);
+                g.DrawLine(pen, 430, 120, 420, 100);
+                //right ear
+                g.DrawLine(pen, 450, 120, 460, 100);
+                g.DrawLine(pen, 470, 130, 460, 100);
+                //left eye
+                g.DrawEllipse(pen, 415, 130, 20, 20);
+                //right eye
+                g.DrawEllipse(pen, 445, 130, 20, 20);
+                //nose
+                g.DrawEllipse(pen, 435, 150, 10, 10);
+                //left whiskers
+                g.DrawLine(pen, 430, 155, 410, 150);
+                g.DrawLine(pen, 430, 155, 410, 155);
+                //right whiskers
+                g.DrawLine(pen, 470, 150, 450, 155);
+                g.DrawLine(pen, 470, 155, 450, 155);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = base.CreateGraphics())
+                g.Clear(Color.White);
+        }
+
+        private void btnActuallyDog_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = base.CreateGraphics())
+            using (Pen pen = new Pen(Color.Black))
+            {
+                g.Clear(Color.White);
+                //head
+                g.DrawEllipse(pen, 400, 120, 80, 80);
+                //body
+                g.DrawEllipse(pen, 390, 200, 100, 120);
+                //left foot
+                g.DrawEllipse(pen, 390, 310, 30, 20);
+                //right foot
+                g.DrawEllipse(pen, 460, 310, 30, 20);
+                //left hand
+                g.DrawEllipse(pen, 390, 230, 30, 20);
+                //right hand
+                g.DrawEllipse(pen, 460, 230, 30, 20);
+                //left ear
+                g.DrawLine(pen, 410, 130, 420, 100);
+                g.DrawLine(pen, 430, 120, 420, 100);
+                //right ear
+                g.DrawLine(pen, 450, 120, 460, 100);
+                g.DrawLine(pen, 470, 130, 460, 100);
+                //left eye
+                g.DrawEllipse(pen, 415, 130, 20, 20);
+                //right eye
+                g.DrawEllipse(pen, 445, 130, 20, 20);
+                //nose
+                g.DrawLine(pen, 435, 155, 435, 175);
+                g.DrawLine(pen, 450, 155, 450, 175);
+                g.DrawLine(pen, 435, 175, 450, 175);
+                g.DrawLine(pen, 435, 175, 440, 180);
+                g.DrawLine(pen, 450, 175, 440, 180);
+            }
+        }
+
+        private void btnTurtle_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = base.CreateGraphics())
+            using (Pen pen = new Pen(Color.Black))
+            {
+                g.Clear(Color.White);
+                //Head
+                g.DrawEllipse(pen, 400, 120, 80, 80);
+                //Body
+                g.DrawEllipse(pen, 405, 200, 120, 150);
+                //left foot
+                g.DrawEllipse(pen, 400, 330, 40, 40);
+                //Right foot
+                g.DrawEllipse(pen, 450, 340, 40, 40);
+                g.DrawEllipse(pen, 390, 220, 40, 40);
+                //Right foot
+                g.DrawEllipse(pen, 450, 220, 40, 40);
+                //left Eye
+                g.DrawEllipse(pen, 415, 140, 15, 6);
+                //right Eye
+                g.DrawEllipse(pen, 450, 140, 15, 6);
+                //staff
+                g.DrawLine(pen, 390, 200, 390, 400);
+                g.DrawEllipse(pen, 385, 180, 10, 20);
+                //Hat
+                g.DrawEllipse(pen, 400, 110, 100, 10);
+                g.DrawLine(pen, 420, 115, 450, 50);
+                g.DrawLine(pen, 480, 115, 450, 50);
+            }
         }
     }
    }
