e83b580ed125c63ed848012bc2bd28fbb8ffa206
Gregory Bell <gregory.bell3@snhu.edu>
2022-10-08 08:14:56 -0400

    final product

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,14 +31,25 @@
             this.components = new System.ComponentModel.Container();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBrushType = new System.Windows.Forms.ComboBox();
+            this.chooseShapeBox = new System.Windows.Forms.ComboBox();
+            this.btnMakeShape = new System.Windows.Forms.Button();
+            this.txtCirHeight = new System.Windows.Forms.Label();
+            this.txtCirWidth = new System.Windows.Forms.Label();
+            this.txtCirY = new System.Windows.Forms.Label();
+            this.txtCirX = new System.Windows.Forms.Label();
+            this.txtShapeHeight = new System.Windows.Forms.TextBox();
+            this.txtShapeWidth = new System.Windows.Forms.TextBox();
+            this.txtShapeY = new System.Windows.Forms.TextBox();
+            this.txtShapeX = new System.Windows.Forms.TextBox();
+            this.btnTurtle = new System.Windows.Forms.Button();
+            this.btnActuallyDog = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.brushSizeBox = new System.Windows.Forms.ComboBox();
             this.btnCat = new System.Windows.Forms.Button();
             this.btnDog = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.btnClear = new System.Windows.Forms.Button();
-            this.btnActuallyDog = new System.Windows.Forms.Button();
-            this.btnTurtle = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,7 +74,19 @@
             // 
             // panel1
             // 
+            this.panel1.AutoSize = true;
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.comboBrushType);
+            this.panel1.Controls.Add(this.chooseShapeBox);
+            this.panel1.Controls.Add(this.btnMakeShape);
+            this.panel1.Controls.Add(this.txtCirHeight);
+            this.panel1.Controls.Add(this.txtCirWidth);
+            this.panel1.Controls.Add(this.txtCirY);
+            this.panel1.Controls.Add(this.txtCirX);
+            this.panel1.Controls.Add(this.txtShapeHeight);
+            this.panel1.Controls.Add(this.txtShapeWidth);
+            this.panel1.Controls.Add(this.txtShapeY);
+            this.panel1.Controls.Add(this.txtShapeX);
             this.panel1.Controls.Add(this.btnTurtle);
             this.panel1.Controls.Add(this.btnActuallyDog);
             this.panel1.Controls.Add(this.btnClear);
@@ -74,8 +97,140 @@
             this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(153, 450);
+            this.panel1.Size = new System.Drawing.Size(155, 747);
             this.panel1.TabIndex = 1;
+            // 
+            // comboBrushType
+            // 
+            this.comboBrushType.FormattingEnabled = true;
+            this.comboBrushType.Items.AddRange(new object[] {
+            "Brush",
+            "Draw Line",
+            "Print Square",
+            "Print Circle"});
+            this.comboBrushType.Location = new System.Drawing.Point(12, 19);
+            this.comboBrushType.Name = "comboBrushType";
+            this.comboBrushType.Size = new System.Drawing.Size(121, 21);
+            this.comboBrushType.TabIndex = 19;
+            this.comboBrushType.Text = "Brush Type";
+            this.comboBrushType.SelectedIndexChanged += new System.EventHandler(this.comboBrushType_SelectedIndexChanged);
+            // 
+            // chooseShapeBox
+            // 
+            this.chooseShapeBox.FormattingEnabled = true;
+            this.chooseShapeBox.Items.AddRange(new object[] {
+            "Oval",
+            "Rectangle"});
+            this.chooseShapeBox.Location = new System.Drawing.Point(11, 286);
+            this.chooseShapeBox.Name = "chooseShapeBox";
+            this.chooseShapeBox.Size = new System.Drawing.Size(121, 21);
+            this.chooseShapeBox.TabIndex = 18;
+            this.chooseShapeBox.Text = "Shape";
+            this.chooseShapeBox.SelectedIndexChanged += new System.EventHandler(this.chooseShapeBox_SelectedIndexChanged);
+            // 
+            // btnMakeShape
+            // 
+            this.btnMakeShape.Location = new System.Drawing.Point(21, 391);
+            this.btnMakeShape.Name = "btnMakeShape";
+            this.btnMakeShape.Size = new System.Drawing.Size(108, 23);
+            this.btnMakeShape.TabIndex = 17;
+            this.btnMakeShape.Text = "Make Shape";
+            this.btnMakeShape.UseVisualStyleBackColor = true;
+            this.btnMakeShape.Click += new System.EventHandler(this.btnMakeShape_Click);
+            // 
+            // txtCirHeight
+            // 
+            this.txtCirHeight.AutoSize = true;
+            this.txtCirHeight.Location = new System.Drawing.Point(85, 349);
+            this.txtCirHeight.Name = "txtCirHeight";
+            this.txtCirHeight.Size = new System.Drawing.Size(44, 13);
+            this.txtCirHeight.TabIndex = 16;
+            this.txtCirHeight.Text = "Height: ";
+            // 
+            // txtCirWidth
+            // 
+            this.txtCirWidth.AutoSize = true;
+            this.txtCirWidth.Location = new System.Drawing.Point(12, 349);
+            this.txtCirWidth.Name = "txtCirWidth";
+            this.txtCirWidth.Size = new System.Drawing.Size(38, 13);
+            this.txtCirWidth.TabIndex = 15;
+            this.txtCirWidth.Text = "Width:";
+            // 
+            // txtCirY
+            // 
+            this.txtCirY.AutoSize = true;
+            this.txtCirY.Location = new System.Drawing.Point(85, 310);
+            this.txtCirY.Name = "txtCirY";
+            this.txtCirY.Size = new System.Drawing.Size(17, 13);
+            this.txtCirY.TabIndex = 14;
+            this.txtCirY.Text = "Y:";
+            // 
+            // txtCirX
+            // 
+            this.txtCirX.AutoSize = true;
+            this.txtCirX.Location = new System.Drawing.Point(12, 310);
+            this.txtCirX.Name = "txtCirX";
+            this.txtCirX.Size = new System.Drawing.Size(17, 13);
+            this.txtCirX.TabIndex = 13;
+            this.txtCirX.Text = "X:";
+            // 
+            // txtShapeHeight
+            // 
+            this.txtShapeHeight.Location = new System.Drawing.Point(88, 365);
+            this.txtShapeHeight.Name = "txtShapeHeight";
+            this.txtShapeHeight.Size = new System.Drawing.Size(40, 20);
+            this.txtShapeHeight.TabIndex = 12;
+            // 
+            // txtShapeWidth
+            // 
+            this.txtShapeWidth.Location = new System.Drawing.Point(21, 365);
+            this.txtShapeWidth.Name = "txtShapeWidth";
+            this.txtShapeWidth.Size = new System.Drawing.Size(46, 20);
+            this.txtShapeWidth.TabIndex = 11;
+            // 
+            // txtShapeY
+            // 
+            this.txtShapeY.Location = new System.Drawing.Point(88, 326);
+            this.txtShapeY.Name = "txtShapeY";
+            this.txtShapeY.Size = new System.Drawing.Size(44, 20);
+            this.txtShapeY.TabIndex = 10;
+            // 
+            // txtShapeX
+            // 
+            this.txtShapeX.Location = new System.Drawing.Point(21, 326);
+            this.txtShapeX.Name = "txtShapeX";
+            this.txtShapeX.Size = new System.Drawing.Size(46, 20);
+            this.txtShapeX.TabIndex = 9;
+            // 
+            // btnTurtle
+            // 
+            this.btnTurtle.Location = new System.Drawing.Point(12, 257);
+            this.btnTurtle.Name = "btnTurtle";
+            this.btnTurtle.Size = new System.Drawing.Size(121, 23);
+            this.btnTurtle.TabIndex = 7;
+            this.btnTurtle.Text = "Wizard Turtle";
+            this.btnTurtle.UseVisualStyleBackColor = true;
+            this.btnTurtle.Click += new System.EventHandler(this.btnTurtle_Click);
+            // 
+            // btnActuallyDog
+            // 
+            this.btnActuallyDog.Location = new System.Drawing.Point(12, 228);
+            this.btnActuallyDog.Name = "btnActuallyDog";
+            this.btnActuallyDog.Size = new System.Drawing.Size(121, 23);
+            this.btnActuallyDog.TabIndex = 6;
+            this.btnActuallyDog.Text = "Dog";
+            this.btnActuallyDog.UseVisualStyleBackColor = true;
+            this.btnActuallyDog.Click += new System.EventHandler(this.btnActuallyDog_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 100);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(121, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // label1
             // 
@@ -125,42 +280,13 @@
             this.toolTip1.Tag = "";
             this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
-            // btnClear
-            // 
-            this.btnClear.Location = new System.Drawing.Point(12, 100);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(121, 23);
-            this.btnClear.TabIndex = 5;
-            this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
-            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-            // 
-            // btnActuallyDog
-            // 
-            this.btnActuallyDog.Location = new System.Drawing.Point(12, 228);
-            this.btnActuallyDog.Name = "btnActuallyDog";
-            this.btnActuallyDog.Size = new System.Drawing.Size(121, 23);
-            this.btnActuallyDog.TabIndex = 6;
-            this.btnActuallyDog.Text = "Dog";
-            this.btnActuallyDog.UseVisualStyleBackColor = true;
-            this.btnActuallyDog.Click += new System.EventHandler(this.btnActuallyDog_Click);
-            // 
-            // btnTurtle
-            // 
-            this.btnTurtle.Location = new System.Drawing.Point(12, 257);
-            this.btnTurtle.Name = "btnTurtle";
-            this.btnTurtle.Size = new System.Drawing.Size(121, 23);
-            this.btnTurtle.TabIndex = 7;
-            this.btnTurtle.Text = "Wizard Turtle";
-            this.btnTurtle.UseVisualStyleBackColor = true;
-            this.btnTurtle.Click += new System.EventHandler(this.btnTurtle_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(828, 451);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Coloring Book App";
@@ -170,6 +296,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -185,6 +312,17 @@
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnActuallyDog;
         private System.Windows.Forms.Button btnTurtle;
+        private System.Windows.Forms.ComboBox chooseShapeBox;
+        private System.Windows.Forms.Button btnMakeShape;
+        private System.Windows.Forms.Label txtCirHeight;
+        private System.Windows.Forms.Label txtCirWidth;
+        private System.Windows.Forms.Label txtCirY;
+        private System.Windows.Forms.Label txtCirX;
+        private System.Windows.Forms.TextBox txtShapeHeight;
+        private System.Windows.Forms.TextBox txtShapeWidth;
+        private System.Windows.Forms.TextBox txtShapeY;
+        private System.Windows.Forms.TextBox txtShapeX;
+        private System.Windows.Forms.ComboBox comboBrushType;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Gregory Bell, Jaleel Gonzales, Levi Bushey
+// Paint app, with ability to draw lines, squares, ovals, as well as outlines for a penguin
+// cat dog and wizard turtle
+// brush function allows different size brushes
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,24 +23,86 @@ namespace Paintapp
             InitializeComponent();
         }
         bool shouldPaint { get; set; } = false;
+        bool printShape { get; set; } = false;
+        bool line { get; set; } = false;
+        int lineX { get; set; } = 0;
+        int lineY { get; set; } = 0;
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             comboBox1.Text = comboBox1.SelectedIndex.ToString();
         }
 
+       
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            shouldPaint = true;
+            using (Graphics g = CreateGraphics())
+            using (Pen mypen = new Pen(Color.Black))
+                //Switch to select brush type (Brush, draw lines, print square or circle
+                switch (comboBrushType.Text)
+            {
+                    //Brush is true, draws colored lines on mouseMove
+                case "Brush":
+                    shouldPaint = true;
+                    break;
+                        //Draw line becomes true and uses the xy of the mouse click as the start of a straight line
+                case "Draw Line":
+                    line = true;
+                    break;
+                        //Prints a small medium or large square at the xy of the mouse click 
+                case "Print Square":
+                        if (brushSizeBox.Text == "Large")
+                        {
+                            g.DrawRectangle(mypen, e.X, e.Y, 100, 100);
+                        }
+                        else if (brushSizeBox.Text == "Medium")
+                        {
+                            g.DrawRectangle(mypen, e.X, e.Y, 60, 60);
+                        }
+                        else
+                        {
+                            g.DrawRectangle(mypen, e.X, e.Y, 30, 30);
+                        }
+                        break;
+                    //Prints a small medium or large circle at the xy of the mouse click 
+                    case "Print Circle":
+                        if (brushSizeBox.Text == "Large")
+                        {
+                            g.DrawEllipse(mypen, e.X, e.Y, 100, 100);
+                        }
+                        else if (brushSizeBox.Text == "Medium")
+                        {
+                            g.DrawEllipse(mypen, e.X, e.Y, 60, 60);
+                        }
+                        else
+                        {
+                            g.DrawEllipse(mypen, e.X, e.Y, 30, 30);
+                        }
+                        break;
+                    default:
+                        //If no choice is made then the default is a brush
+                    shouldPaint = true;
+                    break;
+            }
+            //Records the XY of the mouse click, to be used to draw a straight line ending at 
+            //The XY of mouse up
+            lineX = e.X;
+            lineY = e.Y;
+            
+
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (shouldPaint)
+            using (Graphics g = CreateGraphics())
+            using (SolidBrush myBrush = new SolidBrush(Color.Blue))
+            using (Pen mypen = new Pen(Color.Black))
             {
-                using (Graphics g = CreateGraphics())
-                using (SolidBrush myBrush = new SolidBrush(Color.Blue))
+                //When Brush is chosen shouldpaint is true and on mouse move it draws a colored brush line
+                //comboBox1 decides the color of this brush with the default color being blue
+                if (shouldPaint)
                 {
+
                     switch (comboBox1.Text)
                     {
                         case "Blue":
@@ -63,6 +130,7 @@ namespace Paintapp
                             myBrush.Color = Color.Blue;
                             break;
                     }
+                    //brushSizeBox decides the size of the brush with the default size being medium
                     switch (brushSizeBox.Text)
                     {
                         case "Small":
@@ -78,21 +146,36 @@ namespace Paintapp
                             g.FillEllipse(myBrush, e.X, e.Y, 7, 7);
                             break;
                     }
-                    
+
                 }
             }
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
+            //shouldPaint is false to end the drawn line if brush was chosen
             shouldPaint = false;
+
+            using (Graphics g = CreateGraphics())
+            using (Pen mypen = new Pen(Color.Black))
+            {
+                //if Draw line was chosen a line is drawn 
+                if (line)
+                {//Uses the X,Y, coordinates taken when the mouse is clicked and draws a line
+                 //using the x, y coordinates of the mouse when you release the click
+                    g.DrawLine(mypen, lineX, lineY, e.X, e.Y);
+                }
+            }
+            line = false;
         }
+
 
         private void toolTip1_Popup(object sender, PopupEventArgs e)
         {
   
         }
 
+        //Draws a picture of a PENGUIN. NOT A DOG 
         private void btnDog_Click(object sender, EventArgs e)
         {
             using (Graphics g = base.CreateGraphics())
@@ -121,13 +204,14 @@ namespace Paintapp
             }
         }
 
-
+        //Changes the text in the brushSizeBox to whatever selection was made
         private void brushSizeBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             brushSizeBox.Text = brushSizeBox.SelectedIndex.ToString();
 
         }
 
+        //draws outline of a cat
         private void btnCat_Click(object sender, EventArgs e)
         {
             using (Graphics g = base.CreateGraphics())
@@ -167,12 +251,14 @@ namespace Paintapp
             }
         }
 
+        //Clears the screen
         private void btnClear_Click(object sender, EventArgs e)
         {
             using (Graphics g = base.CreateGraphics())
                 g.Clear(Color.White);
         }
 
+        //Draws the outline of a dog
         private void btnActuallyDog_Click(object sender, EventArgs e)
         {
             using (Graphics g = base.CreateGraphics())
@@ -210,6 +296,7 @@ namespace Paintapp
             }
         }
 
+        //Generate the outline of a wizardly turtle 
         private void btnTurtle_Click(object sender, EventArgs e)
         {
             using (Graphics g = base.CreateGraphics())
@@ -239,6 +326,84 @@ namespace Paintapp
                 g.DrawLine(pen, 420, 115, 450, 50);
                 g.DrawLine(pen, 480, 115, 450, 50);
             }
+        }
+
+
+        //Generate a shape that will be filled with whatever color the brush color drop down box is selected as
+        private void btnMakeShape_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Takes the color, location and size of a shape you want to generate
+                string shapeColor = comboBox1.Text;
+                int coodX = Convert.ToInt32(txtShapeX.Text);
+                int coodY = Convert.ToInt32(txtShapeY.Text);
+                int shapeHeight = Convert.ToInt32(txtShapeHeight.Text);
+                int shapeWidth = Convert.ToInt32(txtShapeWidth.Text);
+
+                using (Graphics g = base.CreateGraphics())
+                using (Pen pen = new Pen(Color.Black))
+                using (SolidBrush myBrush = new SolidBrush(Color.Blue))
+                {
+                    //The shape will be filled with whatever color is chosen in brush color box
+                    switch (comboBox1.Text)
+                    {
+                        case "Blue":
+                            myBrush.Color = Color.Blue;
+                            break;
+                        case "Red":
+                            myBrush.Color = Color.Red;
+                            break;
+                        case "Yellow":
+                            myBrush.Color = Color.Yellow;
+                            break;
+                        case "Green":
+                            myBrush.Color = Color.Green;
+                            break;
+                        case "Purple":
+                            myBrush.Color = Color.Purple;
+                            break;
+                        case "Pink":
+                            myBrush.Color = Color.Pink;
+                            break;
+                        case "Orange":
+                            myBrush.Color = Color.Orange;
+                            break;
+                        default:
+                            myBrush.Color = Color.Blue;
+                            break;
+                    }
+                    //This box chooses between a rectangle and an oval
+                    switch (chooseShapeBox.Text)
+                    {
+                        case "Rectangle":
+                            g.FillRectangle(myBrush, coodX, coodY, shapeWidth, shapeHeight);
+                            break;
+                        case "Oval":
+                            g.FillEllipse(myBrush, coodX, coodY, shapeWidth, shapeHeight);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            //try catch to handle format exceptions for the inputs
+            catch (FormatException)
+            {
+                MessageBox.Show("Entry must be an integer", "Entry Error");
+                txtShapeX.Focus();
+            }
+        }
+
+        //Changes text in the Box for shape 
+        private void chooseShapeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chooseShapeBox.Text = chooseShapeBox.SelectedIndex.ToString();
+        }
+        //Changes text in the Box for brush type
+        private void comboBrushType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBrushType.Text = comboBrushType.SelectedIndex.ToString();
         }
     }
    }
